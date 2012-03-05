95b4575a0a1e9e793dcc8bd2e1b3542023b5fa06
Kevin Zurawel <kzurawel@gmail.com>
2012-03-05 09:27:35 -0500

    Update settings to allow for host-independent file paths

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 # Django settings for topplr project.
+import os
+CWD = os.getcwd()
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -45,7 +47,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/Users/zurawel/topplr/media'
+MEDIA_ROOT = CWD + '/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -72,7 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/zurawel/topplr/static',
+    CWD + '/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -107,7 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/zurawel/topplr/templates',
+    CWD + '/templates',
 )
 
 INSTALLED_APPS = (
