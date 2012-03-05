3841af782a7758b9257358ce8476a86220cb341a
Kevin Zurawel <kzurawel@gmail.com>
2012-03-05 12:23:00 -0500

    Point all list URLs to the same template

--- urls.py
+++ urls.py
@@ -8,10 +8,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', PostListView.as_view(model=Post)),
-    url(r'^news/?', PostListView.as_view(model=NewsPost, queryset=NewsPost.objects.all())),
-    url(r'^photos/?', PostListView.as_view(model=PhotoPost, queryset=PhotoPost.objects.all())),
-    url(r'^videos/?', PostListView.as_view(model=VideoPost, queryset=VideoPost.objects.all())),
-    url(r'^quotes/?', PostListView.as_view(model=QuotePost, queryset=QuotePost.objects.all())),
+    url(r'^news/?', PostListView.as_view(model=NewsPost, queryset=NewsPost.objects.all(), template_name="blog/post_list.html")),
+    url(r'^photos/?', PostListView.as_view(model=PhotoPost, queryset=PhotoPost.objects.all(), template_name="blog/post_list.html")),
+    url(r'^videos/?', PostListView.as_view(model=VideoPost, queryset=VideoPost.objects.all(), template_name="blog/post_list.html")),
+    url(r'^quotes/?', PostListView.as_view(model=QuotePost, queryset=QuotePost.objects.all(), template_name="blog/post_list.html")),
     # Examples:
     # url(r'^$', 'topplr.views.home', name='home'),
     # url(r'^topplr/', include('topplr.foo.urls')),
